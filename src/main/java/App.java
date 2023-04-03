073231e60f875ce379177fefacf1f19c5f1c476e
Igor Golubev <prostogar@gmail.com>
2023-04-03 15:17:31 +0300

    First commit

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld beanHello1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHello1.getMessage());
+        HelloWorld beanHello2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHello2.getMessage());
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getName());
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getName());
+
+        System.out.println(beanHello1 == beanHello2);
+        System.out.println(beanCat1 == beanCat2);
+    }
+}
